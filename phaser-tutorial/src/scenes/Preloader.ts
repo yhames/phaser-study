3b1648dd2c1f9f707f62b95b1f133c347903e282
yhames <wjddn494@naver.com>
2025-03-04 22:51:04 +0900

    Refactor animation creation for mobs, players, effects, attacks, and experience items in Preloader

--- phaser-tutorial/src/scenes/Preloader.ts
+++ phaser-tutorial/src/scenes/Preloader.ts
@@ -88,134 +88,132 @@ export default class Preloader extends Phaser.Scene {
 
     // Create animations
     {
-      {
-        // MOBS
-        this.anims.create({
-          key: Animes.mob1.key,
-          frames: this.anims.generateFrameNumbers(Sprites.mob1.key),
-          frameRate: 12,
-          repeat: -1,
-        });
-        this.anims.create({
-          key: Animes.mob2.key,
-          frames: this.anims.generateFrameNumbers(Sprites.mob2.key),
-          frameRate: 12,
-          repeat: -1,
-        });
-        this.anims.create({
-          key: Animes.mob3.key,
-          frames: this.anims.generateFrameNumbers(Sprites.mob3.key),
-          frameRate: 12,
-          repeat: -1,
-        });
-        this.anims.create({
-          key: Animes.mob4.key,
-          frames: this.anims.generateFrameNumbers(Sprites.mob4.key),
-          frameRate: 12,
-          repeat: -1,
-        });
-        this.anims.create({
-          key: Animes.lion.key,
-          frames: this.anims.generateFrameNumbers(Sprites.lion.key),
-          frameRate: 12,
-          repeat: -1,
-        });
-        this.anims.create({
-          key: Animes.lion.idle,
-          frames: this.anims.generateFrameNumbers(Sprites.lion.key, {
-            start: 0,
-            end: 0,
-          }),
-          frameRate: 1,
-          repeat: 0,
-        });
+      // MOBS
+      this.anims.create({
+        key: Animes.mob1.key,
+        frames: this.anims.generateFrameNumbers(Sprites.mob1.key),
+        frameRate: 12,
+        repeat: -1,
+      });
+      this.anims.create({
+        key: Animes.mob2.key,
+        frames: this.anims.generateFrameNumbers(Sprites.mob2.key),
+        frameRate: 12,
+        repeat: -1,
+      });
+      this.anims.create({
+        key: Animes.mob3.key,
+        frames: this.anims.generateFrameNumbers(Sprites.mob3.key),
+        frameRate: 12,
+        repeat: -1,
+      });
+      this.anims.create({
+        key: Animes.mob4.key,
+        frames: this.anims.generateFrameNumbers(Sprites.mob4.key),
+        frameRate: 12,
+        repeat: -1,
+      });
+      this.anims.create({
+        key: Animes.lion.key,
+        frames: this.anims.generateFrameNumbers(Sprites.lion.key),
+        frameRate: 12,
+        repeat: -1,
+      });
+      this.anims.create({
+        key: Animes.lion.idle,
+        frames: this.anims.generateFrameNumbers(Sprites.lion.key, {
+          start: 0,
+          end: 0,
+        }),
+        frameRate: 1,
+        repeat: 0,
+      });
 
-        // PLAYERS
-        this.anims.create({
-          key: Animes.player.key,
-          frames: this.anims.generateFrameNumbers(Sprites.player.key),
-          frameRate: 12,
-          repeat: -1,
-        });
-        this.anims.create({
-          key: Animes.player.idle,
-          frames: this.anims.generateFrameNumbers(Sprites.player.key, {
-            start: 0,
-            end: 0,
-          }),
-          frameRate: 1,
-          repeat: 0,
-        });
+      // PLAYERS
+      this.anims.create({
+        key: Animes.player.key,
+        frames: this.anims.generateFrameNumbers(Sprites.player.key),
+        frameRate: 12,
+        repeat: -1,
+      });
+      this.anims.create({
+        key: Animes.player.idle,
+        frames: this.anims.generateFrameNumbers(Sprites.player.key, {
+          start: 0,
+          end: 0,
+        }),
+        frameRate: 1,
+        repeat: 0,
+      });
 
-        // EFFECT
-        this.anims.create({
-          key: Animes.explosion.key,
-          frames: this.anims.generateFrameNumbers(Sprites.explosion.key),
-          frameRate: 20,
-          repeat: 0,
-          hideOnComplete: true,
-        });
+      // EFFECT
+      this.anims.create({
+        key: Animes.explosion.key,
+        frames: this.anims.generateFrameNumbers(Sprites.explosion.key),
+        frameRate: 20,
+        repeat: 0,
+        hideOnComplete: true,
+      });
 
-        // ATTACKS
-        this.anims.create({
-          key: Animes.clawWhite.key,
-          frames: this.anims.generateFrameNumbers(Sprites.clawWhite.key),
-          frameRate: 20,
-          repeat: 0,
-          hideOnComplete: true,
-        });
-        this.anims.create({
-          key: Animes.clawYellow.key,
-          frames: this.anims.generateFrameNumbers(Sprites.clawYellow.key),
-          frameRate: 20,
-          repeat: 0,
-          hideOnComplete: true,
-        });
-        this.anims.create({
-          key: Animes.catnip.key,
-          frames: this.anims.generateFrameNumbers(Sprites.catnip.key),
-          frameRate: 20,
-          repeat: -1,
-        });
+      // ATTACKS
+      this.anims.create({
+        key: Animes.clawWhite.key,
+        frames: this.anims.generateFrameNumbers(Sprites.clawWhite.key),
+        frameRate: 20,
+        repeat: 0,
+        hideOnComplete: true,
+      });
+      this.anims.create({
+        key: Animes.clawYellow.key,
+        frames: this.anims.generateFrameNumbers(Sprites.clawYellow.key),
+        frameRate: 20,
+        repeat: 0,
+        hideOnComplete: true,
+      });
+      this.anims.create({
+        key: Animes.catnip.key,
+        frames: this.anims.generateFrameNumbers(Sprites.catnip.key),
+        frameRate: 20,
+        repeat: -1,
+      });
 
-        // EXP UP ITEMS
-        this.anims.create({
-          key: Animes.expUp.red,
-          frames: this.anims.generateFrameNumbers(Sprites.expUp.key, {
-            start: 0,
-            end: 0,
-          }),
-          frameRate: 20,
-          repeat: 0,
-        });
-        this.anims.create({
-          key: Animes.expUp.blue,
-          frames: this.anims.generateFrameNumbers(Sprites.expUp.key, {
-            start: 1,
-            end: 1,
-          }),
-          frameRate: 20,
-          repeat: 0,
-        });
-        this.anims.create({
-          key: Animes.expUp.yellow,
-          frames: this.anims.generateFrameNumbers(Sprites.expUp.key, {
-            start: 2,
-            end: 2,
-          }),
-          frameRate: 20,
-          repeat: 0,
-        });
-        this.anims.create({
-          key: Animes.expUp.green,
-          frames: this.anims.generateFrameNumbers(Sprites.expUp.key, {
-            start: 3,
-            end: 3,
-          }),
-          frameRate: 20,
-          repeat: 0,
-        });
-      }
+      // EXP UP ITEMS
+      this.anims.create({
+        key: Animes.expUp.red,
+        frames: this.anims.generateFrameNumbers(Sprites.expUp.key, {
+          start: 0,
+          end: 0,
+        }),
+        frameRate: 20,
+        repeat: 0,
+      });
+      this.anims.create({
+        key: Animes.expUp.blue,
+        frames: this.anims.generateFrameNumbers(Sprites.expUp.key, {
+          start: 1,
+          end: 1,
+        }),
+        frameRate: 20,
+        repeat: 0,
+      });
+      this.anims.create({
+        key: Animes.expUp.yellow,
+        frames: this.anims.generateFrameNumbers(Sprites.expUp.key, {
+          start: 2,
+          end: 2,
+        }),
+        frameRate: 20,
+        repeat: 0,
+      });
+      this.anims.create({
+        key: Animes.expUp.green,
+        frames: this.anims.generateFrameNumbers(Sprites.expUp.key, {
+          start: 3,
+          end: 3,
+        }),
+        frameRate: 20,
+        repeat: 0,
+      });
     }
   }
 }
